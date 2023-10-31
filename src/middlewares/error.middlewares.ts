8da957c665acb6dd9bc2cc2f8f58c9aeef59c7b1
tranhabaolong608@gmail.com <tranhabaolong608@gmail.com>
2023-10-31 15:22:18 +0700

    fix set header

--- src/middlewares/error.middlewares.ts
+++ src/middlewares/error.middlewares.ts
@@ -6,7 +6,7 @@ import { ErrorWithStatus } from '~/models/Errors'
 export const defaultErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
   // lỗi từ các nơi dồn về đây
   if (err instanceof ErrorWithStatus) {
-    res.status(err.status).json(omit(err, ['status']))
+    return res.status(err.status).json(omit(err, ['status']))
   }
   // nếu mà lỗi xuống đc đây
   // set name, stack, message về numerable true
